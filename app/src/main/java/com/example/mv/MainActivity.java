a820008d0922edd3c684456c272d5786afbb7174
TobyYorktech <102384420+TobyYorktech@users.noreply.github.com>
2022-06-03 09:31:27 -0400

    6/3/22
    Added the first version of converter

---
+++ app/src/main/java/com/example/mv/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = (WebView) findViewById(R.id.webview);
+        webview.loadUrl("file:///android_asset/APP/index.html");
+
+    }
+}
